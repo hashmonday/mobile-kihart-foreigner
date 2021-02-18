47c20a5400f478c0e0069a5becc0f0f1381cbe1e
Suwicha Khaosamlee <hashmonday@gmail.com>
2021-02-18 13:49:06 +0700

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '358263a45804b7b4bfc124eca208ad52'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '96213b8e-3275-4f6f-8364-41a5c9a1c35b'
+};
